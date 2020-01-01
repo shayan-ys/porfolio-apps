d4683fdf62d54b941afbf8bb13dc0d0758f738de
Shayan Ys <shayan.yousefian1372@gmail.com>
2020-01-01 17:38:23 -0500

    add Todo List

---
+++ todo/precache-manifest.e7f971df3e165802c93046a81f481122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53cd57e9462c32693db55c13007f274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f96c9e03f3fd4b9bfe7",
+    "url": "/static/css/main.ba6d64ec.chunk.css"
+  },
+  {
+    "revision": "2df708a656538a6ca1f3",
+    "url": "/static/js/2.06293ee8.chunk.js"
+  },
+  {
+    "revision": "16a4527db387cf31fd19131d298aca07",
+    "url": "/static/js/2.06293ee8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f96c9e03f3fd4b9bfe7",
+    "url": "/static/js/main.8276f8e4.chunk.js"
+  },
+  {
+    "revision": "03a0013317e6f2a8a99d",
+    "url": "/static/js/runtime-main.5c7f07e5.js"
+  }
+]);
